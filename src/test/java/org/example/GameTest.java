7263a81e14a68d834473ad4066e33d9ebeff2808
alekseialenkin <124128504+alekseialenkin@users.noreply.github.com>
2024-02-15 16:47:50 +0300

    added test for out of field

--- src/test/java/org/example/GameTest.java
+++ src/test/java/org/example/GameTest.java
@@ -9,17 +9,24 @@ class GameTest {
     static {
         makeTestsFiles();
     }
+
     @Test
     public void normalTest() {
         int[] result = Game.makeTurn(Util.makeNewField(Util.readFile(FIRST_FILE_NAME)));
         Assertions.assertArrayEquals(FIST_TEST_RESULT, result);
-        System.out.println("Координата х = " + result[0] + " ,координата у = " + result[1]);
+        printResult(result);
     }
 
     @Test
     public void testWithStartPosition() {
         int[] result = Game.makeTurn(Util.makeNewField(Util.readFile(SECOND_FILE_NAME)));
         Assertions.assertArrayEquals(SECOND_TEST_RESULT, result);
-        System.out.println("Координата х = " + result[0] + " ,координата у = " + result[1]);
+        printResult(result);
+    }
+
+    @Test
+    public void outOfField() {
+        int[] result = Game.makeTurn(Util.makeNewField(Util.readFile(THIRD_FILE_NAME)));
+        printResult(result);
     }
 }
--- src/test/java/org/example/GameTestData.java
+++ src/test/java/org/example/GameTestData.java
@@ -7,12 +7,17 @@ import java.util.List;
 public class GameTestData {
     public static final String FIRST_FILE_NAME = "test1.txt";
     public static final String SECOND_FILE_NAME = "test2.txt";
+    public static final String THIRD_FILE_NAME = "test3.txt";
     public static final int[] FIST_TEST_RESULT = new int[]{1, 2};
     public static final int[] SECOND_TEST_RESULT = new int[]{6, 1};
+    public static final int[] THIRD_TEST_RESULT = new int[]{0, 0};
 
     public static void makeTestsFiles() {
-        try (FileWriter firstTestWriter = new FileWriter(FIRST_FILE_NAME); FileWriter secondTestWriter = new FileWriter(SECOND_FILE_NAME)) {
-            List<String> parameters = List.of("10", "10", "0", "0", "right", "right", "go", "right", "go", "right", "go", "left", "left", "go", "left", "go");
+        try (FileWriter firstTestWriter = new FileWriter(FIRST_FILE_NAME);
+             FileWriter secondTestWriter = new FileWriter(SECOND_FILE_NAME);
+             FileWriter thirdTestWriter = new FileWriter(THIRD_FILE_NAME)) {
+            List<String> parameters = List.of("10", "10", "0", "0", "right", "right", "go", "right", "go", "right",
+                    "go", "left", "left", "go", "left", "go");
             for (String s : parameters) {
                 firstTestWriter.write(s + "\n");
             }
@@ -20,8 +25,15 @@ public class GameTestData {
             for (String s : parameters) {
                 secondTestWriter.write(s + "\n");
             }
+            parameters = List.of("10", "10", "0", "0", "go");
+            for (String s : parameters) {
+                thirdTestWriter.write(s + "\n");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    public static void printResult(int [] result){
+        System.out.println("Координата х = " + result[0] + " ,координата у = " + result[1]);
     }
 }
