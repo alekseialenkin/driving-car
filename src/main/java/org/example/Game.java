a420fbe8e6da9c487fdba5641c0ece007b647771
alekseialenkin <124128504+alekseialenkin@users.noreply.github.com>
2024-02-15 16:06:55 +0300

    reformatted class : deleted main method and split logic to different methods

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -10,9 +10,8 @@ public class Game {
     private static int x = 0;
     private static int y = 0;
     private static int direction = 0;
-    private static Field field;
 
-    private static void move() {
+    public static void move(Field field) {
         switch (direction) {
             case 0:
                 if (y > 0) {
@@ -37,12 +36,13 @@ public class Game {
         }
     }
 
-    private static void makeTurn(Field field) {
+    public static int[] makeTurn(Field field) {
+        int[] result = new int[2];
         for (int i = 0; i < field.getCommands().size(); i++) {
             String command = field.getCommands().get(i);
             switch (command) {
                 case "go" -> {
-                    move();
+                    move(field);
                     direction = 0;
                 }
                 case "left" -> direction = (direction + 3) % 4;
@@ -50,29 +50,34 @@ public class Game {
                 default -> System.out.println("Неизвестная команда");
             }
         }
+        result[0] = x;
+        result[1] = y;
+        return result;
     }
 
-    public static void main(String[] args) {
-        try (BufferedReader bf = new BufferedReader(new FileReader("arguments.txt"))) {
-            List<String> parameters = new ArrayList<>();
+    public static List<String> readFile(String name) {
+        List<String> parameters = new ArrayList<>();
+        try (BufferedReader bf = new BufferedReader(new FileReader(name))) {
             String line = bf.readLine();
             while (line != null) {
                 parameters.add(line);
                 line = bf.readLine();
             }
-            if (!parameters.isEmpty()) {
-                int length = Integer.parseInt(parameters.get(0));
-                int height = Integer.parseInt(parameters.get(1));
-                int startLength = Integer.parseInt(parameters.get(2));
-                int startHeight = Integer.parseInt(parameters.get(3));
-                field = new Field(length, height, startLength, startHeight, parameters.subList(4, parameters.size()));
-            } else {
-                System.out.println("Параметры в файле не найдены");
-            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        makeTurn(field);
-        System.out.println("Координата х = " + x + " ,координата у = " + y);
+        return parameters;
+    }
+
+    public static Field makeNewField(List<String> parameters) {
+        if (!parameters.isEmpty()) {
+            int length = Integer.parseInt(parameters.get(0));
+            int height = Integer.parseInt(parameters.get(1));
+            int startLength = Integer.parseInt(parameters.get(2));
+            int startHeight = Integer.parseInt(parameters.get(3));
+            return new Field(length, height, startLength, startHeight, parameters.subList(4, parameters.size()));
+        } else {
+            throw new RuntimeException("Параметры в файле не найдены");
+        }
     }
 }
